0204c3d2b0ce7c8cf2cfdd3a9c4bfa446bca9342
Christian Giraldo Ospina <christian.giraldo.1990@gmail.com>
2020-06-21 22:36:14 -0500

    the initial configuration of the project is done

---
+++ src/main/java/com/masivian/test/CleanCodeTestApplication.java
@@ -0,0 +1,13 @@
+package com.masivian.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CleanCodeTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CleanCodeTestApplication.class, args);
+	}
+
+}
